9f688a23ea6aa83ac620f50ba4a615815fa840ba
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-04-13 09:17:32 +0200

    inizio

---
+++ sketch_apr13a/sketch_apr13a.ino
@@ -0,0 +1,22 @@
+int btn1;
+int btn2;
+int btn3;
+int btn4;
+int btn5;
+int b00;
+int b01;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
+  pinMode(btn3, INPUT);
+  pinMode(btn4, INPUT);
+  pinMode(btn5, INPUT);
+  pinMode(b00, INPUT);
+  pinMode(b01, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
