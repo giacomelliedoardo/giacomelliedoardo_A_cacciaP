d70a78fd3235a70d3ff23bc9c7ec24602d83054d
Edoardo <edoardo.giacomelli@barsanti.edu.it>
2019-05-15 13:12:20 +0200

    funzionante senza bonus e malus

--- sketch_apr13a/sketch_apr13a.ino
+++ sketch_apr13a/sketch_apr13a.ino
@@ -6,7 +6,6 @@ int btn3;
 int btn4;
 int btn5;
 int b00;
-int b01;
 int vite;
 int punti;
 int record;
@@ -15,13 +14,15 @@ byte Pigreco[8] = {B00000, B01100, B10011, B01010, B01010, B01010, B01010, B0101
 
 void setup() {
   // put your setup code here, to run once:
-  b00    = 1;
-  b01    = 2;
-  btn1   = 3;
-  btn2   = 4;
-  btn3   = 5;
-  btn4   = 6;
-  btn5   = 7;
+  lcd.init();
+  lcd.backlight();
+  
+  b00    = 2;
+  btn1   = 4;
+  btn2   = 6;
+  btn3   = 8;
+  btn4   = 10;
+  btn5   = 12;
   vite   = 5;
   punti  = 0;
   record = 0;
@@ -32,43 +33,44 @@ void setup() {
   pinMode(btn4, INPUT);
   pinMode(btn5, INPUT);
   pinMode(b00, INPUT);
-  pinMode(b01, INPUT);
 
   lcd.createChar(0, Cuore);
   lcd.createChar(1, Pigreco);
+  lcd.setCursor(0,0);
+  lcd.print("Inizia a giocare");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  vite = 5;
+  punti = 0;
   inizio();
-  if (vite > 0) {
+  while (vite > 0) {
     int pos = compare();
     int premuto = assegno();
     game(pos, premuto);
   }
-  else {
+    lcd.clear();
     lcd.setCursor(2, 0);
     lcd.print("...!GAME!...");
     lcd.setCursor(3, 1);
     lcd.print("..!OVER!..");
-  }
 }
 
 void inizio() {
   while (digitalRead(b00) != HIGH) {}
-
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.write(byte(0));
   lcd.setCursor(1, 0);
-  lcd.print("=");
-  lcd.setCursor(3, 0);
+  lcd.print(" =");
+  lcd.setCursor(4, 0);
   lcd.print(vite);
   lcd.setCursor(8, 0);
-  lcd.print("Match: ");
+  lcd.print("Punti: ");
   lcd.setCursor(15, 0);
   lcd.print(punti);
-
 }
 
 int assegno() {
@@ -96,11 +98,12 @@ int assegno() {
     }
     delay(1);
   }
+  lcd.clear();
   return pos;
 }
 
 int compare() {
-  int rnd = random(1, 5);
+  int rnd = random(1, 6);
   int pos = 0;
   if (rnd == 1) {
     pos = 0;
@@ -129,5 +132,15 @@ void game(int P, int B) {
   }
   else {
     vite = vite - 1;
-  }
+  } 
+  lcd.setCursor(0, 0);
+  lcd.write(byte(0));
+  lcd.setCursor(1, 0);
+  lcd.print(" =");
+  lcd.setCursor(4, 0);
+  lcd.print(vite);
+  lcd.setCursor(8, 0);
+  lcd.print("Punti: ");
+  lcd.setCursor(14, 0);
+  lcd.print(punti);
 }
